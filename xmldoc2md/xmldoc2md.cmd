21af5ec2bdfdaf023cabe92d39294e81f8605484
Tobias Kiertscher <dev@mastersign.de>
2016-07-24 19:05:15 +0200

    improved the CMD wrapper

--- xmldoc2md/xmldoc2md.cmd
+++ xmldoc2md/xmldoc2md.cmd
@@ -1,1 +1,3 @@
-@powershell -NoProfile -ExecutionPolicy Unrestricted -Command "& '%~dp0\xmldoc2md.ps1' "%*
+@ECHO OFF
+ECHO.XML DOC to Markdown Converter
+powershell -NoLogo -NoProfile -ExecutionPolicy Unrestricted -Command "& '%~dp0\xmldoc2md.ps1' "%*
